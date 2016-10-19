4c5282fec5f5ada9eafd6ff8eb247754b43267b3
v-xucxia <v-xucxia@PC-WINFFEE>
2016-10-19 13:34:54 +0800

    Add project files.

---
+++ TradingViewSample/js/main.js
@@ -0,0 +1,24 @@
+﻿// Your code here!
+
+window.onload = function (evt) {
+    var myChart = new TradingView.widget({
+        "width": 980,
+        "height": 610,
+        "symbol": "NASDAQ:AAPL",
+        "interval": "D",
+        "timezone": "Etc/UTC",
+        "theme": "White",
+        "style": "1",
+        "locale": "en",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "allow_symbol_change": true,
+        "hideideas": true,
+        "show_popup_button": true,
+        "popup_width": "1000",
+        "popup_height": "650"
+    });
+
+}
+
+
